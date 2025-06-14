dfa312bebde82e9cf4993b22cb4d29ce912a8e5e
0tingwei0 <w31272973w@gmail.com>
2025-06-14 21:38:23 +0800

    Update kernel.v

--- rtl/kernel.v
+++ rtl/kernel.v
@@ -15,6 +15,11 @@ module kernel
     input  wire                     sw_rdy,
     output wire [(pDATA_WIDTH-1):0] sw_dat,
 
+    
+    input  wire                     coef_vld,
+    output wire                     coef_rdy,
+    input  wire [(pDATA_WIDTH-1):0] coef_dat, 
+
     input  wire               [7:0] mode,
     input  wire                     decode,
     output wire                     sw_lst  // this is set when handshake
