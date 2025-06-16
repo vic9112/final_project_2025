3250e9755d5c6b39f947672354b6dd8fe6e2e74c
0tingwei0 <w31272973w@gmail.com>
2025-06-17 04:53:13 +0800

    Update butterfly.v

--- rtl/butterfly.v
+++ rtl/butterfly.v
@@ -21,6 +21,74 @@ module butterfly
     output  wire [(pDATA_WIDTH-1):0] bo
 
 );
+    //only for FFT (we want a+bwn, a-bwn)
+    //============================================================================//
+    wire [(pDATA_WIDTH-1):0] bwn; //bwn = bi * omega(n)
+    wire [63:0] bwn_r, bwn_i;
+    wire [(pDATA_WIDTH-1):0] negative_bwn;
+    wire [63:0] negative_bwn_r, negative_bwn_i;
+    //============================================================================//
+    assign bwn_r = bwn[127:64];
+    assign bwn_i = bwn[63:0];
+
+    assign negative_bwn_r = {~bwn_r[63], bwn_r[62:0]};
+    assign negative_bwn_i = {~bwn_i[63], bwn_i[62:0]};
+    assign negative_bwn = {negative_bwn_r,negative_bwn_i};
+
+    //bwn
+    cmul C1( 
+        .num1(bi),//Xr(64bit) + jXi(64bit)
+        .num2(gm),//Yr(64bit) + jYi(64bit)
+        .clk(clk),
+        .rstn(rstn),
+        .result(bwn),
+        .valid(),
+        .ready()
+    );
+
+    //Re{a+bwn}
+    fadd A1_r(
+        .num1(ai[127:64]),
+        .num2(bwn_r),
+        .clk(clk),
+        .rstn(rstn),
+        .result(ao[127:64]),
+        .valid(),
+        .ready()
+    );
+    //Im{a+bwn}
+    fadd A1_i(
+        .num1(ai[63:0]),
+        .num2(bwn_i),
+        .clk(clk),
+        .rstn(rstn),
+        .result(ao[63:0]),
+        .valid(),
+        .ready()
+    );
+
+    //Re{a-bwn}
+    fadd A2_r(
+        .num1(ai[127:64]),
+        .num2(negative_bwn_r),
+        .clk(clk),
+        .rstn(rstn),
+        .result(bo[127:64]),
+        .valid(),
+        .ready()
+    );
+    //Im{a-bwn}
+    fadd A2_i(
+        .num1(ai[63:0]),
+        .num2(negative_bwn_i),
+        .clk(clk),
+        .rstn(rstn),
+        .result(bo[63:0]),
+        .valid(),
+        .ready()
+    );
+
+
 
     // complex mul & add & sub for FFT/iFFT
 
